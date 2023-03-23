4783d9a0bfd4e95dc944af2279e95513d5ea9b01
bilaly <billycsk@gmail.com>
2023-03-23 14:58:54 +0100

    Rajout de fixtures des commentaires

--- src/DataFixtures/CommentFixtures.php
+++ src/DataFixtures/CommentFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Post;
+use App\Entity\User;
 use App\Entity\Comment;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -11,16 +12,26 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 class CommentFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
-    {
+    { 
+        
         $faker = Factory::create('fr_FR');
-        $post = $manager->getRepository(Post::class)->findOneBy([]);
+        $user = new User();
+        $user->setFirstName($faker->firstName);
+        $user->setLastName($faker->lastName);
+        $user->setEmail($faker->safeEmail);
+        $user->setUserName($faker->userName);
+        $user->setPassword($faker->password);
+        $manager->persist($user);
+        $post = new Post();
+        $post->setArticle('Articles test')
+            ->setUser($user);
+        $manager->persist($post);
+       
         for ($i=0; $i < 11 ; $i++) { 
-            if($post != null){
-                $comment = new Comment();
-                $comment->setComment($faker->paragraph);
-                $comment->setPost($post);
-                $manager->persist($comment);
-            }
+            $comment = new Comment();
+            $comment->setComment($faker->paragraph);
+            $comment->setPost($post);
+            $manager->persist($comment);
         }
         // dd($user);
         $manager->flush();
