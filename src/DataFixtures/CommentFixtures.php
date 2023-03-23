00887138ecd5eb4776e086882d4c00fbc532ab39
bilaly <billycsk@gmail.com>
2023-03-23 12:51:48 +0100

    Fixtures load v2

--- src/DataFixtures/CommentFixtures.php
+++ src/DataFixtures/CommentFixtures.php
@@ -1,7 +1,5 @@
 <?php
 
-// src/DataFixtures/CommentFixtures.php
-
 namespace App\DataFixtures;
 
 use Faker\Factory;
@@ -10,24 +8,21 @@ use App\Entity\Comment;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
-
 class CommentFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
         $post = $manager->getRepository(Post::class)->findOneBy([]);
-
-        if ($post !== null) {
-            for ($i=0; $i < 3; $i++) { 
+        for ($i=0; $i < 11 ; $i++) { 
+            if($post != null){
                 $comment = new Comment();
                 $comment->setComment($faker->paragraph);
                 $comment->setPost($post);
                 $manager->persist($comment);
             }
-        $manager->flush();
-        } else {
-            echo "Pas de posts dans la base de données!";
         }
+        // dd($user);
+        $manager->flush();
     }
 }
--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -5,29 +5,30 @@ namespace App\DataFixtures;
 use Faker\Factory;
 use App\Entity\Post;
 use App\Entity\User;
+use App\Entity\Comment;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
-class UserFixtures extends Fixture
+class PostFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+        $user = $manager->getRepository(User::class)->findOneBy([]);
 
         for ($i=0; $i < 11 ; $i++) { 
-            $user = new User();
-            $user->setFirstName($faker->firstName);
-            $user->setLastName($faker->lastName);
-            $user->setEmail($faker->safeEmail);
-            $user->setUserName($faker->userName);
-            $user->setPassword($faker->password);
-            for ($j=0; $j < 3 ; $j++) { 
+            if ($user) {
                 $post = new Post();
                 $post->setArticle($faker->paragraph);
-                $post->setUser($user); 
+                $post->setUser($user);
+                for ($j=0 ; $j<3; $j++) {
+                    $comment = new Comment();
+                    $comment->setComment($faker->paragraph);
+                    $comment->setPost($post);
+                    $manager->persist($comment);
+                }
                 $manager->persist($post);
             }
-            $manager->persist($user);
         }
         // dd($user);
         $manager->flush();
