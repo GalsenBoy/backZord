0d9432a29feafb0131360397c5b613af191d92d2
bilaly <billycsk@gmail.com>
2023-04-03 15:01:52 +0200

    Fixtures load v2

--- src/DataFixtures/PostFixtures.php
+++ src/DataFixtures/PostFixtures.php
@@ -4,8 +4,6 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\Post;
-use App\Entity\User;
-use App\Entity\Comment;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -14,23 +12,13 @@ class PostFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
-        $user = $manager->getRepository(User::class)->findOneBy([]);
-
-        for ($i=0; $i < 11 ; $i++) { 
-            if ($user) {
-                $post = new Post();
-                $post->setArticle($faker->paragraph);
-                $post->setUser($user);
-                for ($j=0 ; $j<3; $j++) {
-                    $comment = new Comment();
-                    $comment->setComment($faker->paragraph);
-                    $comment->setPost($post);
-                    $manager->persist($comment);
-                }
-                $manager->persist($post);
-            }
+        for ($i=0; $i < 6; $i++) { 
+            $post = new Post();
+            $post->setArticle($faker->paragraph);
         }
-        // dd($user);
+        // $product = new Product();
+        // $manager->persist($product);
+
         $manager->flush();
     }
 }
--- src/DataFixtures/TagFixtures.php
+++ src/DataFixtures/TagFixtures.php
@@ -2,36 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tag;
+use App\Repository\PostRepository;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Faker\Factory;
-use App\Entity\Comment;
-use App\Repository\PostRepository;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
-class CommentFixtures extends Fixture implements DependentFixtureInterface
+class TagFixtures extends Fixture implements DependentFixtureInterface
 {
     public function __construct(
         private PostRepository $postRepository
     ){}
     public function load(ObjectManager $manager): void
     {
-        $comments = [];
-      $faker = Factory::create('fr_FR');
-      for ($i=0; $i < 13; $i++) { 
-        $comment = new Comment();
-        $comment->setComment($faker->paragraph);
-        $manager->persist($comment);
-        $comments[] = $comment;
-      }
-      $posts = $this->postRepository->findAll();
+        $tags = [];
+        $faker = Factory::create('fr_FR');
+        for ($i=0; $i <7; $i++) { 
+            $tag = new Tag();
+            $tag->setName($faker->word);
+            $manager->persist($tag);
+            $tags[] = $tag;
+        }
+        $posts = $this->postRepository->findAll();
         foreach ($posts as $post) {
-            $post->addComment(
-                $comments[mt_rand(0,count($comments) -1)]
+            $post->addTag(
+                $tags[mt_rand(0,count($tags) -1)]
             );
         }
         $manager->flush();
     }
+
     public function getDependencies():array{
         return [PostFixtures::class];
     }
